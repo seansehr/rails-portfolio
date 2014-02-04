23324c9da3620f4ee96e294412b01462d38c3c9a
Sean Sehr <sean@seansehr.com>
2014-02-03 23:27:04 -0800

    updating for heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PortfolioRails4::Application.config.secret_key_base = 'ce51cc82aa5b0d7e4a8884a56b1ebb68e97776e45cadfdca147e471e0cf01e7cda2db597268dbc8cbcd4b3045fd083e39da3d71cf742694221f58b33ced470bb'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-PortfolioRails4::Application.config.session_store :cookie_store, key: '_portfolio_rails4_session'
+PortfolioRails4::Application.config.secret_token = if Rails.env.development? or Rails.env.test?
+  ('x' * 30) # meets minimum requirement of 30 chars long
+else
+  ENV['SECRET_TOKEN']
+end
