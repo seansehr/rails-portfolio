097078b206714a06f60d786f647027fc88f281bf
Sean Sehr <sean@seansehr.com>
2014-02-10 22:29:44 -0800

    adding the ability to delete a project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,5 @@
 class ProjectsController < ApplicationController
-  before_action :set_project, only: [:show, :edit, :update]
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
 
   def create
     @project = Project.new(project_params)
@@ -10,6 +10,13 @@ class ProjectsController < ApplicationController
       flash.now[:error] =  "Project could not be saved."
       render :new
     end
+  end
+
+  def destroy
+    project = @project
+    @project.destroy
+    flash[:notice] = "#{project.name} has been deleted."
+    redirect_to projects_url
   end
 
   def edit
---
+++ test/features/projects/deleting_a_project_test.rb
@@ -0,0 +1,9 @@
+require "test_helper"
+
+feature "As the site owner, I want to delete a portfolio item so that I can keep the list focused on my best work" do
+  scenario "deleting a project" do
+    visit project_path(projects(:portfolio))
+    first(:link, "Destroy").click
+    page.text.must_include "has been deleted"
+  end
+end
