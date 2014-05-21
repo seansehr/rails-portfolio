ee240719bff2bf5276b1e172cdf8f1fc1c794bed
Sean Sehr <seansehr@seans-mbp.domain1.smashingideas.com>
2014-05-21 16:48:05 -0700

    Adding posts and users to comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :posts, foreign_key: "author_id"
+  has_many :comments
 
   def author?
     self.role == 'author'
