c2406f95565b1963f3334cf1aa387c475faba973
Sean Sehr <seansehr@seans-mbp.domain1.smashingideas.com>
2014-05-21 16:47:37 -0700

    Adding posts and users to comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140521233647) do
+ActiveRecord::Schema.define(version: 20140521234701) do
 
   create_table "comments", force: true do |t|
     t.string   "author"
@@ -24,7 +24,12 @@ ActiveRecord::Schema.define(version: 20140521233647) do
     t.boolean  "approved"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "post_id"
+    t.integer  "user_id"
   end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "post_comments", force: true do |t|
     t.string   "author"
