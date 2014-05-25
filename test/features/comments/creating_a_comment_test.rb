5bfe732972b3292a961bbab0539cd0c8fd12f6ca
Sean Sehr <seansehr@Seans-MacBook-Pro.local>
2014-05-25 14:05:35 -0700

    Adding tests for project comments

--- test/features/comments/creating_a_comment_test.rb
+++ test/features/comments/creating_a_comment_test.rb
@@ -13,4 +13,16 @@ feature "Creating a comment" do
     page.text.must_include "Comment was successfully created."
     page.text.must_include comments(:comment1).content
   end
+
+  scenario "submit form data to create a new comment as an editor user on a project" do
+    login
+    visit project_path(projects(:portfolio))
+    fill_in "Comment", with: comments(:comment1).content
+
+    # When I submit the form
+    click_on "Submit Comment"
+
+    page.text.must_include "Comment was successfully created."
+    page.text.must_include comments(:comment1).content
+  end
 end
