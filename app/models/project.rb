48944f25a70566180fca4afb380dfa9b34d7662c
Sean Sehr <seansehr@Seans-MacBook-Pro.local>
2014-05-31 17:16:24 -0700

    adding image uploading

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,5 @@ class Project < ActiveRecord::Base
   validates :name, length: { in: 4..255 }
   validates :technologies_used, presence: true
   has_many :comments, as: :commentable
+  mount_uploader :image, ImageUploader
 end
